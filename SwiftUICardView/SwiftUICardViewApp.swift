c37f8de64a1eb77edd00ce0655465965c0be678a
manuaravind <manuaravindpta@gmail.com>
2024-10-08 15:07:57 +0530

    Initial Commit

---
+++ SwiftUICardView/SwiftUICardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICardViewApp.swift
+//  SwiftUICardView
+//
+//  Created by Manu Aravind on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
